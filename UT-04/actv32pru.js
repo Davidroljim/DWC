4da265e003b1344f0cab95dd7f5c93558cd94a8d
davidroljim <davidroldan2901@gmail.com>
2021-11-18 12:32:05 +0100

    furbo

--- UT-04/actv32pru.js
+++ UT-04/actv32pru.js
@@ -4,9 +4,9 @@ var piloto1 = new Object();
 piloto1.nombre = 'David';
 piloto1.escuderia = 'Ferrari';
 
-marca=100;
+marca=70;
 
-var pilotoc1 = new PilotoCarrera (piloto1,marca);
+var pilotoc1 = new PilotoCarrera(piloto1,marca);
 
 var piloto2 = new Object();
 piloto2.nombre = 'Ale';
@@ -19,12 +19,18 @@ var pilotoc2 = new PilotoCarrera (piloto2,marca);
 let sesionPilotos = [pilotoc1,pilotoc2];
 
 function ordenaTiempoPilotos (sesionPilotos){
+
     for (let index = 0; index < sesionPilotos.length; index++) {
-        const element = array[index];
-        
+        sesionPilotos.sort(sesionPilotos[index][2],sesionPilotos[index++][2]);
     }
-    console.log(sesionPilotos);
+    return sesionPilotos;
+
+    /*if (sesionPilotos[0][2]<sesionPilotos[1][2]) {
+        document.write("El piloto 1 tiene menor tiempo que el segundo")
+    }else{
+        document.write("El piloto 1 tiene mayor tiempo que el segundo")
+    }*/
     
 }
 
-ordenaTiempoPilotos(sesionPilotos);
+console.log(ordenaTiempoPilotos(sesionPilotos));
